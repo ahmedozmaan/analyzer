a0cbb4388f7d53eb5bcf9eedff01cb4d8a120b5e
iwangibrahim <irwanbibrahim@onewoorks.com>
2018-09-26 17:18:14 +0800

    update

--- urisys1100_full.py
+++ urisys1100_full.py
@@ -18,9 +18,11 @@ seq = []
 number = 1
 result = {}
 
+
 def cleanResult(result):
     print(result)
     return result[8:14].strip()
+
 
 while True:
     c = ser.readline()
@@ -31,34 +33,34 @@ while True:
             number = number + 1
             if number == 17:
                 testResult = {
-					"device_name": result.get(1)[:23].strip(),
-					"sequence_no": result.get(2)[10:13].strip(),
-					"patient_id": result.get(3)[11:23],
-					"test_date": result.get(4).strip(),
-					"result": {
-						"SG": cleanResult(result.get(6)),
-						"pH": cleanResult(result.get(7)),
-						"LEU": cleanResult(result.get(8)),
-						"NIT": cleanResult(result.get(9)),
-						"PRO": cleanResult(result.get(10)),
-						"GLU": cleanResult(result.get(11)),
-						"KET": cleanResult(result.get(12)),
-						"UBG": cleanResult(result.get(13)),
-						"BIL": cleanResult(result.get(14)),
-						"ERY": cleanResult(result.get(15))
-					}
-				}
+                    "device_name": result.get(1)[:23].strip(),
+                    "sequence_no": result.get(2)[10:13].strip(),
+                    "patient_id": result.get(3)[11:23],
+                    "test_date": result.get(4).strip(),
+                    "result": {
+                        "SG": cleanResult(result.get(6)),
+                        "pH": cleanResult(result.get(7)),
+                        "LEU": cleanResult(result.get(8)),
+                        "NIT": cleanResult(result.get(9)),
+                        "PRO": cleanResult(result.get(10)),
+                        "GLU": cleanResult(result.get(11)),
+                        "KET": cleanResult(result.get(12)),
+                        "UBG": cleanResult(result.get(13)),
+                        "BIL": cleanResult(result.get(14)),
+                        "ERY": cleanResult(result.get(15))
+                    }
+                }
                 print("-------------------------")
                 print(testResult)
                 url = config.MIRTH_SERVER + "/record"
-				print("url : {}".format(url))
+                print("url : {}".format(url))
                 json_data = {
-					"device_id": 1,
-                                        "transaction_code": int(round(time.time() * 1000)),
-					"record_type": "RESULT",
-					"raw_message": "",
-					"message_info": testResult
-				}
+                    "device_id": 1,
+                    "transaction_code": int(round(time.time() * 1000)),
+                    "record_type": "RESULT",
+                    "raw_message": "",
+                    "message_info": testResult
+                }
                 requests.post(url, data=json.dumps(json_data))
                 print("-------------------------")
                 number = 1
